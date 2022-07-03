62fa6596ddb284082b8e3058bb90a36fb220253b
Carlos <litu_@hotmail.com>
2022-07-03 15:06:58 -0300

    variables in .env file

--- api/config.py
+++ api/config.py
@@ -1,12 +1,15 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config(object):
-    JWT_TOKEN = os.environ.get("JWT_TOKEN", "hey_how_lets_go")
-    MONGODB_HOST = os.environ.get("MONGODB_URL","mongodb+srv://carlosanchormongo:sKGs2CWKjJOB6ZZo@cluster0.e11wi.mongodb.net/?retryWrites=true&w=majority")
-    AWS_ACCESS_KEY = os.environ.get("AWS_ACCESS_KEY", "AKIA6AWHAFNGTWA5FUGZ")
-    AWS_SECRET_KEY = os.environ.get("AWS_SECRET_KEY", "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")
-    AWS_BUCKET_NAME = os.environ.get("AWS_S3_BUCKET_NAME", "anchorproject")
-    AWS_S3_HOST = os.environ.get("AWS_S3_HOST", "https://s3.amazonaws.com")
+    JWT_TOKEN = os.getenv('JWT_TOKEN')
+    MONGODB_HOST = os.getenv('MONGODB_URL')
+    AWS_ACCESS_KEY = os.getenv('AWS_ACCESS_KEY')
+    AWS_SECRET_KEY = os.getenv('AWS_SECRET_KEY')
+    AWS_BUCKET_NAME = os.getenv('AWS_S3_BUCKET_NAME')
+    AWS_S3_HOST = os.getenv('AWS_S3_HOST')
 
     QUERY_LIMIT_DEFAULT = 10
     QUERY_PAGE_DEFAULT = 0
